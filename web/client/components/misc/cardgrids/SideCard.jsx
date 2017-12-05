66ad0d75dc23be56bdc46eadc9777a90f224edec
allyoucanmap <bovio.stefano@gmail.com>
2017-12-05 12:10:33 +0100

    Added className to SideCard

--- web/client/components/misc/cardgrids/SideCard.jsx
+++ web/client/components/misc/cardgrids/SideCard.jsx
@@ -9,8 +9,8 @@
 
 const React = require('react');
 
-module.exports = ({onClick=() => {}, title, preview, description, caption, tools, selected, ...more} = {}) =>
-<div className={`mapstore-side-card${selected ? ' selected' : ''}`} onClick={() => onClick({title, preview, description, caption, tools, ...more})}>
+module.exports = ({onClick=() => {}, title, preview, description, caption, tools, selected, className, ...more} = {}) =>
+<div className={`mapstore-side-card${selected ? ' selected' : ''} ` + className} onClick={(event) => onClick({event, title, preview, description, caption, tools, ...more})}>
   <div className="mapstore-side-preview">
       {preview}
   </div>
